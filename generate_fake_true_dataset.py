eec90280affab2e4ba88ae17520bd533f6b65dd8
marluxiabossls <henrique.dasilvagameiro@epfl.ch>
2024-03-06 09:54:03 +0100

    added .py to generate the dataset for the detector

---
+++ generate_fake_true_dataset.py
@@ -0,0 +1,193 @@
+from datasets import load_dataset, load_from_disk, Dataset, DatasetDict, concatenate_datasets
+import numpy as np
+import pandas
+import argparse
+from tqdm import tqdm
+
+from transformers import (AutoModelForCausalLM, AutoTokenizer, BertForSequenceClassification, BertTokenizer, BertModel,
+ RobertaForSequenceClassification, RobertaTokenizer, RobertaModel, TrainingArguments, Trainer)
+
+from genenerator import LLMGenerator
+
+
+def create_random_subset(dataset, n=10):
+    """
+    Create a random subset of the dataset
+    """
+    if n > len(dataset):
+        n = len(dataset)
+    indices = np.random.choice(len(dataset), n, replace=False)
+    subset = dataset.select(indices)
+    return subset
+
+def process_true_dataset(true_dataset, gen_tokenizer,  fake_dataset_size, max_nb_tokens_input=100):
+    #dataset = load_dataset(dataset_path)
+
+    dataset = true_dataset
+
+    # discard instructions that are more than max_nb_tokens_input tokens
+    max_nb_tokens_input = max_nb_tokens_input
+
+    # tokenize the instructions
+    dataset = dataset.map(lambda x: {"tokenized_instruction": gen_tokenizer(x["instruction"])})
+    dataset = dataset.map(lambda x: {"tokenized_context": gen_tokenizer(x["context"])})
+    dataset_before_len = len(dataset["train"])
+    dataset = dataset.filter(lambda x: len(x["tokenized_instruction"]["input_ids"]) + len(x["tokenized_context"]["input_ids"]) <= max_nb_tokens_input)
+    dataset_after_len = len(dataset["train"])
+    print(f"Percent of data discarded: {100*(1 - dataset_after_len/dataset_before_len):.2f}%")
+
+
+    true_dataset = dataset.select_columns(["response"])
+    true_dataset = true_dataset.rename_column("response", "text")
+
+    # select random samples from true_dataset to match fake_dataset size
+    true_dataset = true_dataset.shuffle(seed=42)
+    #true_dataset = true_dataset.select(range(len(fake_dataset["train"])))
+    true_dataset = true_dataset.select(range(fake_dataset_size))
+
+    # create label = 0 for true responses and label = 1 for fake responses
+    true_dataset = true_dataset.map(lambda x: {"label": 0})
+
+    # save dataset
+    #true_dataset.save_to_disk("true_dataset")
+
+    return true_dataset
+
+
+def generate_fake_responses(generator, true_dataset, gen_tokenizer):
+    """
+    Traverse dataset and generate responses for each instruction
+    """
+
+    fake_responses = []
+
+    # TODO: improve this loop by parallelizing/batch
+    for data in tqdm(true_dataset):
+        # Create query in the format that the generator expects
+        text_instruction = f"Context: {data['context']} \n {data['instruction']}"
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"{text_instruction}"},
+        ]
+        text = gen_tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True
+        )
+
+        # Generate response
+        output = generator(text, skip_special_tokens=False)
+        
+        fake_responses.append(output)
+    return fake_responses
+
+def filter_instruction(sample, gen_tokenizer):
+    """
+    Note: only works if special tokens are not removed
+    """
+
+    text_instruction = f"Context: {sample['context']} \n {sample['instruction']}"
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"{text_instruction}"},
+    ]
+    text_template = gen_tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    
+    generated_response = sample["generated_response"]
+
+    response_without_instruction = generated_response.replace(text_template, "")
+    return {"generated_response": response_without_instruction}
+
+def generate_fake_dataset(true_dataset, fake_dataset_size):
+    subset_size = fake_dataset_size
+    train_subset = create_random_subset(true_dataset["train"], n=subset_size)
+
+    fake_responses_train = Dataset.from_dict({"generated_response": fake_responses_train, "instruction": train_subset["instruction"],
+    "context": train_subset["context"], "true_response": train_subset["response"], "category": train_subset["category"]})
+
+    fake_dataset = DatasetDict()
+    fake_dataset["train"] = fake_responses_train
+
+    return fake_dataset
+
+def process_fake_dataset(fake_dataset):
+
+    fake_dataset = fake_dataset.map(lambda x: {"label": 1})
+
+    # remove true_response from fake_dataset
+    fake_dataset = fake_dataset.remove_columns(["true_response"])
+
+    # rename generated_response to response
+    fake_dataset = fake_dataset.rename_column("generated_response", "text")
+    fake_dataset = fake_dataset.select_columns(["text", "label"])
+
+    return fake_dataset
+
+def merge_true_fake_dataset(true_dataset, fake_dataset):
+    
+    merged = concatenate_datasets([true_dataset["train"], fake_dataset["train"]])
+    merged_dataset = DatasetDict()
+    merged_dataset["train"] = merged
+
+    # shuffle the dataset
+    merged_dataset = merged_dataset.shuffle(seed=42)
+
+    # save merged dataset
+    #merged_dataset.save_to_disk("merged_dataset")
+
+    return merged_dataset
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--true_dataset_path", type=str, help="Path to the true dataset (hugginface dataset path)", default="databricks/databricks-dolly-15k")
+    parser.add_argument("--fake_dataset_size", type=int, help="Size of the fake dataset", default=10)
+    parser.add_argument("--max_nb_tokens_input", type=int, help="Max number of tokens for input", default=100)
+    parser.add_argument("--generator", type=str, help="Generator model name between 'QWEN' and TODO", default="qwen")
+    parser.add_argument("--device", type=str, help="Device to use for the generator", default="cuda")
+
+    args = parser.parse_args()
+
+    # load generator
+    if args.generator == "qwen":
+        gen_path = "Qwen/Qwen1.5-0.5B-Chat"
+        gen_model = AutoModelForCausalLM.from_pretrained(gen_path, torch_dtype="auto").to(args.device)
+        gen_tokenizer = AutoTokenizer.from_pretrained(gen_path, trust_remote_code=True)
+        generator = LLMGenerator(gen_model, gen_tokenizer)
+
+    else:
+        # no other generator is supported for now
+        raise ValueError("Generator not supported")
+    
+    # load true dataset
+    true_dataset = load_dataset(args.true_dataset_path)
+
+    # process true dataset
+    true_dataset = process_true_dataset(true_dataset, gen_tokenizer, args.fake_dataset_size, args.max_nb_tokens_input)
+    true_dataset.save_to_disk("true_dataset")
+
+    # generate fake responses
+    fake_responses = generate_fake_responses(generator, true_dataset, gen_tokenizer)
+
+    # generate fake dataset
+    fake_dataset = generate_fake_dataset(true_dataset, args.fake_dataset_size)
+
+    # process fake dataset
+    fake_dataset = process_fake_dataset(fake_dataset)
+    fake_dataset.save_to_disk("fake_dataset")
+
+    # merge true and fake dataset
+    merged_dataset = merge_true_fake_dataset(true_dataset, fake_dataset)
+    merged_dataset.save_to_disk("fake_true_dataset")
+
+
+
+
+    
+
